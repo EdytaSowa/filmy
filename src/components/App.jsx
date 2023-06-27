1a8fe2c3e6047660cb6260dcd62e3c5e12caf0b0
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-06-27 12:56:29 +0200

    changes in Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 
-
 import { Movies } from '../pages/Movies';
 import { NotFound } from '../pages/NotFound';
 import { Home } from '../pages/Home';
@@ -18,30 +17,23 @@ export const App = () => {
         flexDirection: 'column',
         gap: '30px',
         // justifyContent: 'center',
-        paddingTop: "30px",
+        paddingTop: '30px',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
-    
       <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        
-    
-        
-        <Route index element={<Home />} />
-        <Route path="/:movieId" element={<MovieDetails />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
-        
-        <Route path="*" element={<NotFound />} />
-        
-         </Route>
-        
       </Routes>
     </div>
   );
