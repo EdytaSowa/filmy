ae00187319767e6099bef673c36b370a8a64e05d
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-06-27 19:09:05 +0200

    add styles

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -37,15 +37,3 @@ export default Reviews;
 
 
 
-// const handleSubmit = e => {
-//   setSearchParams({ query: e.target[0].value });
-//   e.preventDefault();
-//   return;
-// };
-
-// useEffect(() => {
-//   const movieTitle = searchParams.get('query');
-//   if (movieTitle) {
-//     API.searchMovies(movieTitle).then(ret => setMovies(ret));
-//   }
-// }, [searchParams]);
